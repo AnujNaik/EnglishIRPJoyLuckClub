40846a939f9cb117d8296ade02a85f9530cbd3f4
Anuj Naik <41352184+AnujNaik@users.noreply.github.com>
2021-11-13 19:29:06 -0800

    Updates to Piano Page

--- English Website/pages/assets/js/customscript.js
+++ English Website/pages/assets/js/customscript.js
@@ -2,6 +2,16 @@ const keys = document.querySelectorAll(".key"),
   note = document.querySelector(".nowplaying"),
   hints = document.querySelectorAll(".hints");
 
+var songAudio1 = document.getElementById("songAudio1");
+var songAudio2 = document.getElementById("songAudio2");
+
+var buttonAudio1 = document.getElementById("song1Button");
+var buttonAudio2 = document.getElementById("song2Button");
+
+var song1Playing = false;
+var song2Playing = false;
+
+
 function playNote(e) {
   const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`),
     key = document.querySelector(`.key[data-key="${e.keyCode}"]`);
@@ -23,6 +33,31 @@ function removeTransition(e) {
 
 function hintsOn(e, index) {
   e.setAttribute("style", "transition-delay:" + index * 50 + "ms");
+}
+
+function playSong1() {
+    if (!song1Playing){
+        songAudio1.play()
+        song1Playing = true;
+        buttonAudio1.innerHTML = "Pause"
+    } else {
+        songAudio1.pause();
+        song1Playing = false;
+        buttonAudio1.innerHTML = "Play"
+    }
+    
+}
+
+function playSong2() {
+    if (!song2Playing){
+        songAudio2.play()
+        song2Playing = true;
+        buttonAudio2.innerHTML = "Pause"
+    } else {
+        songAudio2.pause();
+        song2Playing = false;
+        buttonAudio2.innerHTML = "Play"
+    }
 }
 
 hints.forEach(hintsOn);
